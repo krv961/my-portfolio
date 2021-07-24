b11aa2e00e4bdea4d6b9c371fadaa2d736d8d251
user <user@users-iMac.local>
2021-07-23 22:20:26 -0600

    adding some components

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import image from "../sunset.jpeg";
+
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Monstera Leaves"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello.
+        </h1>
+        <br></br>
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          I'm Kevin.
+        </h1>
+      </section>
+    </main>
+  );
+}
